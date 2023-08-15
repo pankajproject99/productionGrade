231e709e02a1078c5235237db7e1fe747f969625
pankajproject99 <pankajproject99@gmail.com>
2023-08-16 01:24:29 +0530

    Exception Controller Feedback1: 1. Add basic exception with try catch and centerialized method

--- src/main/java/com/example/productionGrade/controller/TaskController.java
+++ src/main/java/com/example/productionGrade/controller/TaskController.java
@@ -4,6 +4,7 @@ import com.example.productionGrade.Dto.CreateTaskRequest;
 import com.example.productionGrade.Dto.TaskResponse;
 import com.example.productionGrade.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
+
     private final TaskService taskService;
 
     @Autowired
@@ -25,7 +27,11 @@ public class TaskController {
      */
     @GetMapping
     public List<TaskResponse> getAllTasks() {
-        return taskService.getAllTasks();
+        try {
+            return taskService.getAllTasks();
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving tasks: " + e.getMessage());
+        }
     }
 
     /**
@@ -35,7 +41,11 @@ public class TaskController {
      */
     @PostMapping("/create")
     public TaskResponse createTask(@RequestBody CreateTaskRequest request) {
-        return taskService.createTask(request);
+        try {
+            return taskService.createTask(request);
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating task: " + e.getMessage());
+        }
     }
 
     /**
@@ -45,7 +55,16 @@ public class TaskController {
      */
     @PutMapping("/{id}")
     public ResponseEntity<TaskResponse> markTaskAsCompleted(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(taskService.markTaskAsCompleted(id));
+        try {
+            return ResponseEntity.ok(taskService.markTaskAsCompleted(id));
+        } catch (Exception e) {
+            throw new RuntimeException("Error marking task as completed: " + e.getMessage());
+        }
+    }
+
+    // Global exception handler
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleException(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
-
